098241f381915e03c51d68ab3d4b1f58fe0ef194
samandar-mee <samandarcoderdev@gmail.com>
2023-01-07 13:28:29 +0500

    Ktor and Koin

---
+++ app/src/main/java/com/sdk/koinktortest/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.sdk.koinktortest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sdk.koinktortest.ui.theme.KoinKtorTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KoinKtorTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+//https://github.com/Uzmobiler/KtorExample
